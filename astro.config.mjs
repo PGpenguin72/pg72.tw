3f967e5fa86e881368ead91425b09694517ecd97
PGpenguin72 <88070853+PGpenguin72@users.noreply.github.com>
2026-01-29 01:34:54 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://pg72.tw/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
